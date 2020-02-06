de216645c8e7758f1d8d37d45479342d227f0755
Vadim Dmitriev <dva3213@gmail.com>
2020-02-06 12:13:37 +0300

    Привел HTML в подобающий вид. Создал директорию static

---
+++ toDoList/static/js/script.js
@@ -0,0 +1,7 @@
+function showForm() {
+    document.getElementById("new-task-form").style.display = "block";
+};
+
+function hideForm() {
+    document.getElementById("new-task-form").style.display = "none";
+};
