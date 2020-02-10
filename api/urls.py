6aa289c2be5f5b6cc8e556e33b9596579c5a2795
Vadim Dmitriev <dva3213@gmail.com>
2020-02-10 10:09:48 +0300

    Сделал метод GET /api/tasks/{id} и DELETE /api/tasks/{id}

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "api"
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('tasks/', views.TaskView.as_view()),
+    path('tasks/', views.TasksView.as_view()),
+    path('tasks/<int:id>/', views.TaskView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import User
+from django.http import HttpRequest, Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from toDoList.models import Task
 from .serializers import TaskSerializer
 
-class TaskView(APIView):
+class TasksView(APIView):
     def get(self, request):
-        tasks = Task.objects.filter(owner=request.user.id)
+        tasks = Task.objects.filter(owner=request.user)
         serializer = TaskSerializer(tasks, many=True, read_only=True)
 
         return Response({"tasks": serializer.data})
@@ -20,3 +21,26 @@ class TaskView(APIView):
         if serializer.is_valid(raise_exception=True):
             task_saved = serializer.save()
         return Response({"success": f"Task '{task_saved.title}' created successfully"})
+
+class TaskView(APIView):
+    def get_task(self, id, user):
+        try:
+            task = Task.objects.get(pk=id)
+
+            if task.owner != user:
+                raise Http404
+        
+            return task
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request: HttpRequest, id):
+        task = self.get_task(id, request.user)
+
+        serializer = TaskSerializer(task)
+        return Response({"task": serializer.data})
+
+    def delete(self, request: HttpRequest, id):
+        task = self.get_task(id, request.user)
+        task.delete()
+        return Response({"success": f"Task '{task.title}' deleted!"})
