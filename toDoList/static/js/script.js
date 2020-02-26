2a0d22b60a1ba8d0b8bd846c6e38af26e406b0e6
Vadim Dmitriev <dva3213@gmail.com>
2020-02-26 14:00:31 +0300

    Начал внедрять Vue.js. описал экземпляр и два компонента. Экземпляр взаимодействует с GET /api/tasks

--- toDoList/static/js/script.js
+++ toDoList/static/js/script.js
@@ -1,3 +1,65 @@
+Vue.config.productionTip = false
+
+Vue.component('to-do-list-item', {
+	props: ['task'],
+	data: function() {
+		return {
+			listClass: "list",
+		}
+	},
+	template: `
+	<li>
+		<div>
+			{{ task.id }}
+			<br>
+			{{ task.title }}
+			<br>
+			{{ task.description }}
+		</div>
+	</li>
+	`,
+});
+
+Vue.component('to-do-list', {
+	props: ['tasks'],
+	data: function() {
+		return {
+			listClass: "list",
+			items: ['a', 'b', 'c'],
+		}
+	},
+	template: `
+	<div v-bind:class="listClass">
+		<ul>
+			<to-do-list-item v-for="task in tasks" v-bind:task="task"></to-do-list-item>
+		</ul>
+	</div>`,
+});
+
+new Vue({
+	el: "#app",
+	template: `
+	<div>
+		<h1>Hello World</h1>
+		<to-do-list v-bind:tasks="tasks"></to-do-list>
+	</div>
+	`,
+	data: {
+		tasks: [],
+	},
+	created: function () {
+		fetch('/api/tasks', {
+			method: "GET",
+			headers: { "X-CSRFToken": getCookie('csrftoken'), "Content-Type": "application/json" },
+		}).then(Response => {
+			// JSON.parse(Response.body);
+			Response.json().then(ResponseObj =>{
+				this.tasks = ResponseObj["tasks"];
+			});
+		})
+	},
+});
+
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -12,84 +74,4 @@ function getCookie(name) {
         }
     }
     return cookieValue;
-};
-
-function showForm() {
-    document.getElementById("new-task-form").style.display = "block";
-};
-
-function hideForm() {
-    document.getElementById("new-task-form").style.display = "none";
-};
-
-function deleteTask(id) {
-    fetch("api/tasks/"+id+"/", {
-        method: "DELETE",
-        headers: { "X-CSRFToken": getCookie('csrftoken') }
-    }).then(Response => {
-        if (Response.status == 200) {
-            document.getElementById("task#"+id).remove()
-        };
-    });
-};
-
-function moveTask(id) {
-    fetch("api/tasks/"+id+"/move/", {
-        method: "POST",
-        headers: { "X-CSRFToken": getCookie('csrftoken') }
-    }).then(Response => {
-        if (Response.status == 200) {
-            var task = document.getElementById("task#"+id);
-            if (task.parentElement.id == "doneTasks") {
-                document.getElementById("notDoneTasks").appendChild(task);
-                document.getElementById("moveBtn#"+id).textContent = "Done!"
-            } else {
-                document.getElementById("doneTasks").appendChild(task);
-                document.getElementById("moveBtn#"+id).textContent = "Stil not done!"
-            }
-        };
-    });
-};
-
-function createTask() {
-    title = document.getElementById("title").value;
-    description = document.getElementById("description").value;
-
-    fetch("api/tasks/", {
-        method: "POST",
-        headers: { "X-CSRFToken": getCookie('csrftoken'), "Content-Type": "application/json" },
-        body: JSON.stringify({
-            "task": {
-                "title": title,
-                "description": description,
-                "done": false,
-            }
-        }),
-    }).then(Response => {
-        if (Response.status == 200) {
-            var li = document.createElement("li");
-            var taskDiv = document.createElement("div");
-            taskDiv.innerHTML = title + " " + description + " ";
-            var deleteTaskBtn = document.createElement("button");
-            deleteTaskBtn.innerHTML = "Delete";
-            
-            var doneTaskBtn = document.createElement("button");
-            doneTaskBtn.innerHTML = "Done!";
-
-            Response.json().then(data => {
-                li.id = "task#" + data.taskID;
-                deleteTaskBtn.setAttribute("onclick", "deleteTask(" + data.taskID + ")");
-                doneTaskBtn.setAttribute("onclick", "moveTask(" + data.taskID + ")");
-                doneTaskBtn.id = "moveBtn#" + data.taskID;
-            });
-
-            taskDiv.appendChild(deleteTaskBtn);
-            taskDiv.appendChild(doneTaskBtn);
-
-
-            li.appendChild(taskDiv);
-            document.getElementById("notDoneTasks").appendChild(li);
-            hideForm();
-        }
-    });
 };
